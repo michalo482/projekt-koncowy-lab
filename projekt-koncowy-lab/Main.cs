62901168fd271e9a77932a64d24406342ee550bd
DrNocere <arturpetrylak@gmail.com>
2022-04-06 12:38:13 +0200

    update 04/06/22 -A
    Project created

---
+++ projekt-koncowy-lab/Main.cs
@@ -0,0 +1,10 @@
+﻿namespace projekt_koncowy_lab
+{
+    class Hello
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello World!");
+        }
+    }
+}
